977d0292552dfa3c6dce93245388eb53daed3c6f
yudaiyano <kemurin.yudai0723@gmail.com>
2019-07-31 22:53:19 +0900

    create destroy function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,6 +35,12 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to root_url, notice: "ユーザー削除しました"
+  end
+
   private
 
     def user_params
