fa51c6b022431985c6386b35393788d93ba40d6a
yudaiyano <kemurin.yudai0723@gmail.com>
2019-07-31 16:17:07 +0900

    create user_model

---
+++ db/migrate/20190731065925_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :password_digest
+      t.string :word
+      t.string :url
+
+      t.timestamps
+    end
+  end
+end
