a9e975d85453b182bd88d3595f83e873de2161b3
yudaiyano <kemurin.yudai0723@gmail.com>
2019-07-31 21:42:22 +0900

    add create function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,9 +11,22 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to user_url(@user), notice: "登録しました"
+    else
+      render :new
+    end
   end
 
   def edit
   end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation, :word, :url)
+    end
 
 end
