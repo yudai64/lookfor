64a3b53af08cc4c58b77daa904d6db6651f114f1
yudaiyano <kemurin.yudai0723@gmail.com>
2019-08-02 17:53:13 +0900

    difine currnet_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user
+
+  private
+
+    #ログインしているユーザー所得する
+    def current_user
+      @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    end
 end
