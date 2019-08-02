08079181056a29932a0092c5afba15adbef476be
yudaiyano <kemurin.yudai0723@gmail.com>
2019-08-02 15:08:16 +0900

    create login create function

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,23 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    @user = User.find_by(email: session_params[:email])
+
+    if @user&.authenticate(session_params[:password])
+      session[:user_id] = @user.id
+      flash[:success] = "ログインしました"
+      redirect_to @user
+    else
+      flash.now[:danger] = "メールとパスワードの組み合わせが正しくありません"
+      render :new
+    end
+  end
+
+  private
+
+    def session_params
+      params.require(:session).permit(:email, :password)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'users#new'
   resources :users
-  get '/login', to: 'sessions#new'
+  get '/login',  to: 'sessions#new'
+  post '/login', to: 'sessions#create'
 end
