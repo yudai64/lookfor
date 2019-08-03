4728a02d78168c063711fc91aaa40e798a5302e7
yudaiyano <kemurin.yudai0723@gmail.com>
2019-08-03 15:32:46 +0900

    modify application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,19 @@
 class ApplicationController < ActionController::Base
   add_flash_types :success, :info, :warning, :danger
-  helper_method :current_user, :logged_in?
+  helper_method :current_user, :logged_in?, :current_user?
   before_action :authenticate_user
 
   private
+
+  #ユーザーログインする
+  def login_user(user)
+    session[:user_id] = user.id
+  end
+
+  #与えられたユーザーが、今ログインしているユーザーならtrueを返す
+  def current_user?(user)
+    user == current_user
+  end
 
   #ログインしているユーザー所得する
   def current_user
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,2 @@
 module ApplicationHelper
-  private
-
-  #ユーザーログインする
-  def login_user(user)
-    session[:user_id] = user.id
-  end
-
-  #与えられたユーザーが、今ログインしているユーザーならtrueを返す
-  def current_user?(user)
-    user == current_user
-  end
-
 end
