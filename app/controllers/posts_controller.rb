a0743f4611ee954c4c505086db50dd6349bb3cf3
yudaiyano <kemurin.yudai0723@gmail.com>
2019-08-06 20:19:30 +0900

    create posts create function

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,29 @@
+class PostsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+    @post = current_user.posts.new
+  end
+
+  def create
+    @post = current_user.posts.new(post_params)
+    if @post.save
+      redirect_to posts_path, success: '投稿しました'
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  private
+
+    def post_params
+      params.require(:post).permit(:title, :description)
+    end
+end
