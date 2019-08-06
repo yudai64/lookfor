c62cc022768f7ab6bd499172d8bc93506b0aebc2
yudaiyano <kemurin.yudai0723@gmail.com>
2019-08-06 21:28:45 +0900

    configure time_zone

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,8 @@ module Lookfor
       g.test_framework false
     end
 
+    config.time_zone = 'Asia/Tokyo'
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
