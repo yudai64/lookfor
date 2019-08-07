015f97c90172ad9d58c4aa68357e019462efac1d
yudaiyano <kemurin.yudai0723@gmail.com>
2019-08-08 00:02:25 +0900

    modify files

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,7 +12,7 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = current_user.posts.new(post_params)
+    @post = current_user.posts.build(post_params)
     if @post.save
       redirect_to posts_path, success: '投稿しました'
     else
