ca4dd4f554c7342300eb65bee8eddb02f2fc9a24
yudaiyano <kemurin.yudai0723@gmail.com>
2019-09-02 23:19:25 +0900

    template engine setting

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,8 @@ module Lookfor
 
     config.time_zone = "Asia/Tokyo"
 
+    config.generators.template_engine = :slim
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
