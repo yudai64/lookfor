9ddd62f95ab3c7a72aadd625c16bb8d942c1949d
yudaiyano <kemurin.yudai0723@gmail.com>
2019-09-04 01:56:58 +0900

    add upload function

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -42,7 +42,7 @@ class PostsController < ApplicationController
   private
 
     def post_params
-      params.require(:post).permit(:title, :description)
+      params.require(:post).permit(:title, :description, :image)
     end
 
     def find_post
