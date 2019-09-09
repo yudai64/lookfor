cc8f269513e66dfd2281eb331e516fa94a62b67d
yudaiyano <kemurin.yudai0723@gmail.com>
2019-09-09 18:58:50 +0900

    remove password_digest

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_09_084131) do
+ActiveRecord::Schema.define(version: 2019_09_09_095107) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,7 +58,6 @@ ActiveRecord::Schema.define(version: 2019_09_09_084131) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.string "password_digest"
     t.string "word"
     t.string "url"
     t.datetime "created_at", null: false
