eb92c8de5376c03419ce1bb1992755f28aa8a922
yudaiyano <kemurin.yudai0723@gmail.com>
2019-09-09 20:50:22 +0900

    add name field

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,14 @@
 class ApplicationController < ActionController::Base
   add_flash_types :success, :info, :warning, :danger
   helper_method :current_user, :logged_in?, :current_user?
+  protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :age])
+    end
 
     # ユーザーログインする
     def login_user(user)
