80d941e1c13347baa9faa0dd31583b49d27877dc
yudaiyano <kemurin.yudai0723@gmail.com>
2019-09-12 01:32:23 +0900

    modified Layout error by rubocop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,29 +10,4 @@ class ApplicationController < ActionController::Base
       devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :word, :url])
       devise_parameter_sanitizer.permit(:account_update, keys: [:name, :word, :url])
     end
-
-    # ユーザーログインする
-    # def login_user(user)
-      # session[:user_id] = user.id
-    # end
-
-    # 与えられたユーザーが、今ログインしているユーザーならtrueを返す
-    # def current_user?(user)
-      # user == current_user
-    # end
-
-    # ログインしているユーザー所得する
-    # def current_user
-      # @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
-    # end
-
-    # ログインしていればtrueしていなければfalseを返す
-    # def logged_in?
-      # current_user.present?
-    # end
-
-    # ログイン済みユーザーかどうか確認
-    # def authenticate_user
-      # redirect_to login_path, danger: "ログインしてください" unless logged_in?
-    # end
 end
