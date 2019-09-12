3794e814d020a15377cefb1a0ad74e2189649e7a
yudaiyano <kemurin.yudai0723@gmail.com>
2019-09-12 13:22:53 +0900

    deleted unnecessary files

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,21 +7,6 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-  def new
-    @user = User.new
-  end
-
-  def create
-    @user = User.new(user_params)
-
-    if @user.save
-      login_user(@user)
-      redirect_to profile_path, success: "登録しました"
-    else
-      render :new
-    end
-  end
-
   private
 
     def user_params
