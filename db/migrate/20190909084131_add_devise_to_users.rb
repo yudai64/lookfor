17bb205b5cfa892ea73a6940d8198a1ebb9fc4bd
yudaiyano <kemurin.yudai0723@gmail.com>
2019-09-12 19:03:51 +0900

    add index to user table

--- db/migrate/20190909084131_add_devise_to_users.rb
+++ db/migrate/20190909084131_add_devise_to_users.rb
@@ -37,9 +37,9 @@ class AddDeviseToUsers < ActiveRecord::Migration[5.2]
       # end
     end
 
-    # add_index :users, :email,                unique: true
-    # add_index :users, :reset_password_token, unique: true
-    # add_index :users, :confirmation_token,   unique: true
+    add_index :users, :email,                unique: true
+    add_index :users, :reset_password_token, unique: true
+    add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -70,6 +70,9 @@ ActiveRecord::Schema.define(version: 2019_09_09_095107) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
