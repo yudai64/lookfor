0422a153070fca115ca137cbb6169260f242f330
yudaiyano <kemurin.yudai0723@gmail.com>
2019-09-16 08:02:27 +0900

    prepared omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: "users/registrations",
     sessions: "users/sessions",
+    omniauth_callbacks: "users/omniauth_callbacks"
   }
-
   devise_scope :user do
     get "sign_in", to: "users/sessions#new"
     get "sign_out", to: "users/sessions#destroy"
