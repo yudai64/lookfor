13816c46c30a1bdd6864aedf665da6e59e559a74
yudaiyano <kemurin.yudai0723@gmail.com>
2019-09-17 13:08:02 +0900

    setting line login

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  def line; basic_action end
   # You should configure your model like this:
   # devise :omniauthable, omniauth_providers: [:twitter]
 
@@ -27,4 +28,30 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   # def after_omniauth_failure_path_for(scope)
   #   super(scope)
   # end
+  private
+
+  def basic_action
+    @omniauth = request.env['omniauth.auth']
+    if @omniauth.present?
+      @profile = User.where(provider: @omniauth['provider'], uid: @omniauth['uid']).first
+      if @profile
+        @profile.set_values(@omniauth)
+        sign_in(:user, @profile)
+      else
+        @profile = User.new(provider: @omniauth['provider'], uid: @omniauth['uid'])
+        email = @omniauth['info']['email'] ? @omniauth['info']['email'] : "#{@omniauth['uid']}-#{@omniauth['provider']}@example.com"
+        @profile = current_user || User.create!(provider: @omniauth['provider'], uid: @omniauth['uid'], email: email, name: @omniauth['info']['name'], password: Devise.friendly_token[0, 20])
+        @profile.set_values(@omniauth)
+        sign_in(:user, @profile)
+        # redirect_to edit_user_path(@profile.user.id) and return
+      end
+    end
+    flash[:notice] = "ログインしました"
+    redirect_to root_path
+  end
+
+
+  def fake_email(uid,provider)
+    return "#{auth.uid}-#{auth.provider}@example.com"
+  end
 end
