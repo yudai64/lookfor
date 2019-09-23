b4426da3196f1942fe4be448b1021124200f8aaf
yudaiyano <kemurin.yudai0723@gmail.com>
2019-09-23 22:57:59 +0900

    modified line login setting

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,26 +1,15 @@
 # frozen_string_literal: true
 
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  def line; basic_action end
+  def line
+    @user = User.from_omniauth(request.env["omniauth.auth"])
 
-  private
-
-    def basic_action
-     @omniauth = request.env['omniauth.auth']
-     if @omniauth.present?
-       @profile = User.where(provider: @omniauth['provider'], uid: @omniauth['uid']).first
-       if @profile
-         @profile.set_values(@omniauth)
-         sign_in(:user, @profile)
-       else
-         @profile = User.new(provider: @omniauth['provider'], uid: @omniauth['uid'])
-         @profile = current_user || User.create!(provider: @omniauth['provider'], uid: @omniauth['uid'], name: @omniauth['info']['name'], password: Devise.friendly_token[0, 20])
-         @profile.set_values(@omniauth)
-         sign_in(:user, @profile)
-         # redirect_to edit_user_path(@profile.user.id) and return
-       end
-     end
-     flash[:notice] = "ログインしました"
-     redirect_to root_path
+    if @user.persisted?
+      flash[:notice] = "ログインしました。"
+      sign_in_and_redirect @user
+    else
+      session["devise.#{line}_data"] = request.env["omniauth.auth"]
+      redirect_to new_user_registration_url
     end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,25 +11,18 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
 
-  def social_profile(provider)
-    social_profiles.select{ |sp| sp.provider == provider.to_s }.first
+  def self.from_omniauth(auth)
+    user = User.where(uid: auth.uid, provider: auth.provider).first
+
+    unless user
+      user = User.create(
+        uid: auth.uid,
+        provider: auth.provider,
+        password: Devise.friendly_token[0, 20],
+        name: auth.name
+      )
+    end
+
+    user
   end
-
-  def set_values(omniauth)
-    return if provider.to_s != omniauth['provider'].to_s || uid != omniauth['uid']
-    credentials = omniauth['credentials']
-    info = omniauth['info']
-
-    access_token = credentials['refresh_token']
-    access_secret = credentials['secret']
-    credentials = credentials.to_json
-    name = info['name']
-    # self.set_values_by_raw_info(omniauth['extra']['raw_info'])
-  end
-
-  def set_values_by_raw_info(raw_info)
-    self.raw_info = raw_info.to_json
-    self.save!
-  end
-
 end
