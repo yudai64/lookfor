696696a522e44b2b48a61931bef04d471481972f
yudaiyano <kemurin.yudai0723@gmail.com>
2019-09-23 17:01:01 +0900

    modified migration

--- db/migrate/20190920082219_change_column_to_user.rb
+++ db/migrate/20190920082219_change_column_to_user.rb
@@ -2,5 +2,6 @@ class ChangeColumnToUser < ActiveRecord::Migration[5.2]
   def change
     add_column :users, :provider, :string
     add_column :users, :uid, :string
+    add_index :users, [:provider, :uid], unique: true
   end
 end
