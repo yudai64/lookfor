39d4e4ad12db22515d82e3bd9ced473c2ca06236
yudaiyano <kemurin.yudai0723@gmail.com>
2019-09-27 13:38:29 +0900

    modified rails error by rubocop

--- db/migrate/20190920082219_change_column_to_user.rb
+++ db/migrate/20190920082219_change_column_to_user.rb
@@ -1,7 +1,9 @@
 class ChangeColumnToUser < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :provider, :string
-    add_column :users, :uid, :string
+    change_table :users, bulk: true do |t|
+      t.string :provider
+      t.string :uid
+    end
     add_index :users, [:provider, :uid], unique: true
   end
 end
