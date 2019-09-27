ecb43207787d685a0a857ff1349eae34a40b1de4
yudaiyano <kemurin.yudai0723@gmail.com>
2019-09-27 18:07:03 +0900

    modified login_helper

--- spec/support/login_helper.rb
+++ spec/support/login_helper.rb
@@ -2,8 +2,7 @@ module LoginHelpers
   # ログインする
   def login(user)
     visit new_user_session_path
-    fill_in "メールアドレス", with: user.email
-    fill_in "パスワード", with: user.password
     click_button " Log in "
+    click_button "ログイン"
   end
 end
